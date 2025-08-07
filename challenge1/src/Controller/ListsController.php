cb0a9e9f92e594e6212cf1f7a1a9dc8f199817bd
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-07 15:46:16 -0400

    feat: create List

---
+++ challenge1/src/Controller/ListsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\CreateListDTO;
+use App\Repository\ListsRepository;
+use App\Service\ValidatorService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class ListsController extends AbstractController
+{
+    private ListsRepository $listsRepository;
+    private ValidatorService $validator;
+
+    public function __construct(ListsRepository $listsRepository, ValidatorService $validator)
+    {
+        $this->listsRepository = $listsRepository;
+        $this->validator = $validator;
+    }
+
+    #[Route('/lists/{id}', name: 'get_list', methods: ['GET'])]
+    public function getList(): JsonResponse
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/lists', name: 'crete_list', methods: ['POST'])]
+    public function createList(Request $req, SerializerInterface $serializer): JsonResponse
+    {
+        /** @var CreateListDTO $data */
+        $data = $serializer->deserialize($req->getContent(), CreateListDTO::class, 'json');
+
+        $errors = $this->validator->validate($data);
+
+        if (!empty($errors)) {
+            return $this->json(['errors' => $errors], 422);
+        }
+
+        $list = $this->listsRepository->createList($data);
+
+        return $this->json($list);
+    }
+}
