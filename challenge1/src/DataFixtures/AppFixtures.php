1631b78c76ea5bda64b9af132b6bc327b06bd5d1
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-09 18:09:56 -0400

    chore: load roles

--- challenge1/src/DataFixtures/AppFixtures.php
+++ challenge1/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Permission;
+use App\Entity\Role;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,20 +11,28 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
         $this->loadPermissions($manager)
-            ->loadSuperUser($manager);
+            ->loadSuperUser($manager)
+            ->loadRoles($manager);
 
         $manager->flush();
     }
 
+    private function loadRoles(ObjectManager $manager): static
+    {
+        $roles = Role::getRoles();
+        foreach ($roles as $r) {
+            $role = new Role();
+            $role->setName($r['name']);
+            $role->setDescription($r['description']);
+            $manager->persist($role);
+        }
+        return $this;
+    }
+
     private function loadPermissions(ObjectManager $manager): static
     {
-        $permissions = [[
-            'name' => 'CREATE_USERS',
-            'description' => 'Create and register new users',
-        ]];
+        $permissions = Permission::getPermissions();
         foreach ($permissions as $perm) {
             $permission = new Permission();
             $permission->setName($perm['name']);
