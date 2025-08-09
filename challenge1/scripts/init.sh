86f748b3be79ff5405f5523e67a0021379442b98
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-08 21:37:46 -0400

    wip: docker updated

--- challenge1/scripts/init.sh
+++ challenge1/scripts/init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-php bin/console doctrine:database:create --if-not-exists
+cleanup() {
+    echo "Stoping server"
+    symfony server:stop
+    exit 0
+}
+
+symfony server:stop >/dev/null 2>&1
+
+php bin/console doctrine:database:create --if-not-exists >/dev/null 2>&1
 
 mkdir -p migrations
-php bin/console doctrine:migrations:diff --from-empty-schema
+php bin/console doctrine:migrations:diff --from-empty-schema >/dev/null 2>&1
 
-php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction >/dev/null 2>&1
 
-php bin/console doctrine:fixtures:load --append --no-interaction
+php bin/console doctrine:fixtures:load --append --no-interaction >/dev/null 2>&1
 
 symfony server:start --port=8080 --allow-all-ip
