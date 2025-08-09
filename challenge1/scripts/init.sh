fa6caa73091ed3c63ab63db5b7eb3cf735aaf7ec
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-08 20:41:38 -0400

    chore: init script

--- challenge1/scripts/init.sh
+++ challenge1/scripts/init.sh
@@ -1,9 +1,4 @@
-#!/bin/env sh
-
-until pg_isready -h database -U user -d db; do
-    echo "Esperando o banco de dados..."
-    sleep 2
-done
+#!/bin/bash
 
 php bin/console doctrine:database:create --if-not-exists
 
@@ -13,3 +8,5 @@ php bin/console doctrine:migrations:diff --from-empty-schema
 php bin/console doctrine:migrations:migrate --no-interaction
 
 php bin/console doctrine:fixtures:load --append --no-interaction
+
+symfony server:start --port=8080 --allow-all-ip
