1eb313a84d3bae27bbd66a4885445a1c62d8b0d9
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-10 21:01:47 -0400

    chore: file name changed

--- challenge1/src/Entity/Permission.php
+++ challenge1/src/Entity/Permission.php
@@ -2,13 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\PermissionsRepository;
+use App\Repository\PermissionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: PermissionsRepository::class)]
+#[ORM\Entity(repositoryClass: PermissionRepository::class)]
 #[ORM\Table(name: '`permissions`')]
 class Permission
 {
