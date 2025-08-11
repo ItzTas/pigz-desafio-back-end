df5b41e02d46f75b7dca56c40dc057a9b7aeeae7
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-10 21:02:12 -0400

    chore: name changed

--- challenge1/src/Repository/PermissionRepository.php
+++ challenge1/src/Repository/PermissionRepository.php
@@ -2,18 +2,18 @@
 
 namespace App\Repository;
 
-use App\Entity\Permissions;
+use App\Entity\Permission;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Permissions>
  */
-class PermissionsRepository extends ServiceEntityRepository
+class PermissionRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Permissions::class);
+        parent::__construct($registry, Permission::class);
     }
 
     //    /**
