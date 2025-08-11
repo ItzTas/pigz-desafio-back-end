3fa2fdb08b8a53ea96bd3aa47c7c8e0ad6843c1b
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-10 23:46:21 -0400

    chore: jwt routes still not working

--- challenge1/src/Controller/Users/UsersController.php
+++ challenge1/src/Controller/Users/UsersController.php
@@ -8,27 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class UsersController extends AbstractController
 {
-    // #[Route('/login', name: 'login', methods: ['POST'])]
-    // public function index(): JsonResponse
-    // {
-    //     return $this->json([
-    //         'message' => 'Welcome to your new controller!',
-    //         'path' => 'src/Controller/UsersController.php',
-    //     ]);
-    // }
-
-    #[Route('/register', name: 'register_user', methods: ['GET'])]
+    #[Route('/register', name: 'register_user', methods: ['POST'])]
     public function registerUser(): JsonResponse
     {
         return $this->json([]);
-    }
-
-    #[Route('/login/superuser', name: 'get_superuser', methods: ['GET'])]
-    public function superUser(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UsersController.php',
-        ]);
     }
 }
--- challenge1/src/MiddlewareInterfaces/TokenAuthenticatedControler.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace App\MiddlewareInterfaces;
-
-interface TokenAuthenticatedControler {}
